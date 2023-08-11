3bb30d2cccd8c93cd69b08ae2c744f68aafd4163
rizafahlevi <54127172+rizafahlevi@users.noreply.github.com>
2023-08-11 10:58:27 +0700

    Tes.py

--- Tes.py
+++ Tes.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style('darkgrid')
 pal = sns.color_palette()
